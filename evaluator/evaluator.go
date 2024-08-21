b806f3b24a59b14c73ad261412cfb41db917393c
ta-02 <talal.gl2004@gmail.com>
2024-08-21 13:56:09 -0400

    added in eval for int literals

---
+++ evaluator/evaluator.go
@@ -0,0 +1,32 @@
+package evaluator
+
+import (
+	"github.com/ta-02/c-like-interpreter/ast"
+	"github.com/ta-02/c-like-interpreter/object"
+)
+
+func Eval(node ast.Node) object.Object {
+	switch node := node.(type) {
+
+	case *ast.Program:
+		return evalStatements(node.Statements)
+
+	case *ast.ExpressionStatement:
+		return Eval(node.Expression)
+
+	case *ast.IntegerLiteral:
+		return &object.Integer{Value: node.Value}
+	}
+
+	return nil
+}
+
+func evalStatements(stmts []ast.Statement) object.Object {
+	var result object.Object
+
+	for _, statement := range stmts {
+		result = Eval(statement)
+	}
+
+	return result
+}
